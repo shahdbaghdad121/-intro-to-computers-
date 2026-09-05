4b666d3a960f453dc4f375ed7aea3d347dd20cbf
shahdbaghdad121 <shahdbaghdad121@gmail.com>
2026-09-05 22:58:57 +0300

    Create 09-countdown.cpp

---
+++ حلقات-التكرار/09-countdown.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 10;
+    
+    while (n > 0) {
+        cout << n << ", ";
+        n--;
+    }
+    
+    cout << "FIRE!";
+    return 0;
+}
