1db46700b9fa55132615357e2d799bf62302a0fc
shahdbaghdad121 <shahdbaghdad121@gmail.com>
2026-09-05 22:56:55 +0300

    Add grade switch program in C++

---
+++ بنى-التحكم/06-grade-switch.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int grade;
+    
+    cout << "Enter a grade: ";
+    cin >> grade;
+    
+    switch (grade / 10) {
+        case 10:
+        case 9: cout << "A"; break;
+        case 8: cout << "B"; break;
+        case 7: cout << "C"; break;
+        case 6: cout << "D"; break;
+        default: cout << "Failed";
+    }
+    
+    return 0;
+}
